8b75bf4d0cdf7b3b1cd25d691d5dd0a02d4ef991
greyspectrum <hello@greyspectrum.io>
2020-08-29 11:10:56 -0400

    finish bash one-liner

--- client/src/main.rs
+++ client/src/main.rs
@@ -3,7 +3,9 @@ use std::process::Command;
 fn main() {
     let output = Command::new("curl")
         .arg("-v")
-        .arg("https://api.trustedservices.intel.com/sgx/certification/v1/pckcrl?ca={processor}")
+        .arg(
+            r#""https://api.trustedservices.intel.com/sgx/certification/v1/pckcrl?ca={processor}""#,
+        )
         .arg("2>&1")
         .arg("|")
         .arg("awk")
@@ -12,6 +14,17 @@ fn main() {
         .arg(r#"'{print $2}'"#)
         .arg("|")
         .arg("sed")
+        .arg("-e")
+        .arg(":a")
+        .arg("-e")
+        .arg(r#"'s@%@\\x@g;/./,$!d;/^\n*$/{$d;N;};/\n$/ba'"#)
+        .arg("|")
+        .arg("xargs")
+        .arg("-0")
+        .arg("printf")
+        .arg(r#""%b""#)
+        .arg(">")
+        .arg("pck_chain.pem")
         .output()?;
 
     if !output.status.success() {
