fbbe0144e6c1578c7ed811478e5abba167f950d0
greyspectrum <hello@greyspectrum.io>
2020-08-29 15:09:24 -0400

    curl works, stdout redirection does not

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,30 +1,57 @@
-use std::process::Command;
+use std::io::Read;
+use std::process::{Command, Stdio};
 
 fn main() {
-    Command::new("curl")
+    let mut buf = [0u8; 11];
+
+    let child1 = Command::new("curl")
         .arg("-v")
-        .arg(
-            r#""https://api.trustedservices.intel.com/sgx/certification/v1/pckcrl?ca={processor}""#,
-        )
-        .arg("2>&1")
-        .arg("|")
-        .arg("awk")
+        .arg("https://api.trustedservices.intel.com/sgx/certification/v1/pckcrl?ca={processor}")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("fail");
+
+    let child2 = Command::new("tac")
+        .stdin(child1.stdout.unwrap())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("fail");
+
+    let child3 = Command::new("tac")
+        .stdin(child2.stdout.unwrap())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("fail");
+
+    let child4 = Command::new("awk")
         .arg("-F")
         .arg(r#""SGX-PCK-CRL-Issuer-Chain: ""#)
         .arg(r#"'{print $2}'"#)
-        .arg("|")
-        .arg("sed")
+        .stdin(child3.stdout.unwrap())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("fail");
+
+    let child5 = Command::new("sed")
         .arg("-e")
         .arg(":a")
         .arg("-e")
         .arg(r#"'s@%@\\x@g;/./,$!d;/^\n*$/{$d;N;};/\n$/ba'"#)
-        .arg("|")
-        .arg("xargs")
+        .stdin(child4.stdout.unwrap())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("fail");
+
+    let child6 = Command::new("xargs")
         .arg("-0")
         .arg("printf")
         .arg(r#""%b""#)
-        .arg(">")
-        .arg("pck_chain.pem")
-        .output()
-        .expect("failed to execute process");
+        .stdin(child5.stdout.unwrap())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("fail");
+
+    child6.stdout.unwrap().read(&mut buf).expect("fail");
+
+    println!("output: {:?}", String::from_utf8(buf.to_vec()).unwrap());
 }
