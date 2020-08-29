f26af3eb555fc0b16b9fac809cac80b5fc3f564d
greyspectrum <hello@greyspectrum.io>
2020-08-29 10:47:34 -0400

    curl pck cert chain

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,20 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let output = Command::new("curl")
+        .arg("-v")
+        .arg("https://api.trustedservices.intel.com/sgx/certification/v1/pckcrl?ca={processor}")
+        .arg("2>&1")
+        .arg("|")
+        .arg("awk")
+        .arg("-F")
+        .arg(r#""SGX-PCK-CRL-Issuer-Chain: ""#)
+        .arg(r#"'{print $2}'"#)
+        .arg("|")
+        .arg("sed")
+        .output()?;
+
+    if !output.status.success() {
+        error_chain::bail!("Command executed with failing error code");
+    }
 }
